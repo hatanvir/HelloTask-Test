783541e60b46e25db9d1f850278918b5442470cb
Dreamploy <dreamploy2017@gmail.com>
2020-09-08 23:53:14 +0600

    Initial

---
+++ app/src/main/java/com/tvr/easynote/bottom_nav_fragment/FavoriteFragment.java
@@ -0,0 +1,24 @@
+package com.tvr.easynote.bottom_nav_fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tvr.easynote.R;
+public class FavoriteFragment extends Fragment {
+
+
+    public FavoriteFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_favorite, container, false);
+    }
+}
---
+++ app/src/main/java/com/tvr/easynote/bottom_nav_fragment/NoteFragment.java
@@ -0,0 +1,25 @@
+package com.tvr.easynote.bottom_nav_fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tvr.easynote.R;
+
+
+public class NoteFragment extends Fragment {
+
+    public NoteFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_note, container, false);
+    }
+}
